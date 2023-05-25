ebd1f3449ddba64fd1dd7c229af72f5ba8b06a4e
PiroAvni <avnipiro.dev@gmail.com>
2023-05-25 15:56:18 +0100

    add fetch response hero api

--- src/components/SearchWidget/index.jsx
+++ src/components/SearchWidget/index.jsx
@@ -1,42 +1,40 @@
-import React, { useState, useEffect } from "react";
-// import heroApi from '../../api/heroApi';
+// import React, { useState, useEffect } from "react";
+// // import heroApi from '../../api/heroApi';
 
-import { SearchForm } from "../";
+// import { SearchForm } from "../";
 
-export default function SearchWidget() {
-//   const [compareHero, setcompareHero] = useState(false);
-  const [heroData, setHeroData] = useState([]);
-  const [search, setSearch] = useState("superman");
+// export default function SearchWidget() {
 
+//   const [heroData, setHeroData] = useState([]);
+//   const [name, setName] = useState("superman");
 
+  
+//   useEffect(() => {
+//     async function searchHeroAPI() {
+//       try {
+//         const response = await fetch(
+//           `https://superheroapi.com/api/10160846917000330/search/${name}`
+//         );
+//         const rawData = await response.json();
+//         const data = rawData.map((h) => h.hero);
+//         setHeroData(data);
+//       } catch (error) {
+//         console.log("Error occurred during API request:", error);
+//       }
+//     }
 
+//     searchHeroAPI();
+//   }, [name]);
 
+//   function handleSearch(userInput) {
+//     setName(userInput);
+//     console.log(userInput);
+//   }
 
-  useEffect(() => {
-    async function searchHeroAPI() {
-      const response = await fetch(
-        `https://superheroapi.com/api/search/${search}`
-      );
-      const rawData = await response.json();
-      const data = rawData.map((hero) => hero.hero);
-      setHeroData(data);
-    }
-
-    searchHeroAPI();
-  }, [search]);
-
-  function handleSearch(userInput) {
-    setSearch(userInput);
-  }
-
-console.log('SEARCH:', search)
-
-  return (
-    <>
-      <SearchForm handleSearch={handleSearch} />
-      {/* {
-    {<HeroList heroData={heroData} /> }
-      } */}
-    </>
-  );
-}
+//   console.log(heroData);
+//   return (
+//     <>
+//       <SearchForm handleSearch={handleSearch} />
+//     </>
+//   );
+// }
--- src/layout/NavBar/index.jsx
+++ src/layout/NavBar/index.jsx
@@ -1,22 +1,48 @@
-
+import React, { useState, useEffect } from "react";
+import axios from "axios"
 import { SearchForm } from '../../components'
-import logo from '../../../public/images/logo (3).png'
+import logo from '../../../public/images/logo.png'
 import './style.css'
 
 const Navbar = () => {
+  const [heroData, setHeroData] = useState([]);
+  const [name, setName] = useState("superman");
+  const [isLoading, setIsLoading] = useState(true);
 
-    const handleSearch = (searchTerm) => {
-      
-      console.log('Searching for:', searchTerm);
-    };
+  useEffect(() => {
+    async function searchHeroAPI() {
+      try {
+       
+        const response = await axios.get(
+          `https://superheroapi.com/api.php/10160846917000330/search/${name}`
+        )
+        console.log(response)
+        const results = await response.results;
+        setIsLoading(false)
+          // const data = results.map((h) => h.hero);
+        setHeroData(results);
+     
+      } catch (error) {
+        console.log("Error occurred during API request:", error);
+      }
+      setIsLoading(false)
+    }
+
+    searchHeroAPI();
+  }, []);
   
+  function handleSearch(userInput) {
+    setName(userInput);
+    console.log(userInput);
+  }
+  console.log(heroData);
     return (
       <nav className="navbar">
         <div className="navbar-logo">
           <img src={logo} alt="Logo" />
         </div>
         <div className="navbar-search">
-          <SearchForm onSearch={handleSearch} />
+        <SearchForm handleSearch={handleSearch} isLoading={isLoading} />
         </div>
       </nav>
     );
