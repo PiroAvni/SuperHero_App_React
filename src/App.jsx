e2b6df097b0832ed7493146b6b2dc073e8434d6b
Diego Ramos <diegorramos84@gmail.com>
2023-05-25 10:34:53 +0100

    install axios, add states and useeffect to app.jsx page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import './App.css'
 
 function App() {
+  const [userHero, setUserHero] = useState('')
+  const [pcHero, setPcHero] = useState('')
 
+  useEffect(() => {
+    const randomId = Math.floor(Math.random() * 701)
+    console.log(randomId)
+
+    const fetchHero = async () => {
+
+      try {
+        const response = await axios.get(`https://www.superheroapi.com/api.php/6403898876320150/${randomId}`)
+        console.log(response)
+        const hero = await response.data
+        setPcHero(hero)
+      } catch (error) {
+        console.log(error.message)
+      }
+    }
+
+    fetchHero()
+  }, [])
+
+  console.log(pcHero)
 
   return (
     <>
-     
+
     </>
   )
 }
